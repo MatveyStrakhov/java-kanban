e763bb07b602420b2b8fd534cda8150cb46db563
MStrakhov <matveystrakhov@yandex.ru>
2023-05-12 13:19:08 +0300

    Project 5 V 3.1

--- src/model/Node.java
+++ src/model/Node.java
@@ -2,6 +2,13 @@ package model;
 
 public class Node<T> {
     private T data;
+    private Node<T> prev;
+    private Node<T> next;
+    public Node(T data, Node<T> prev, Node<T> next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
 
     public T getData() {
         return data;
@@ -26,14 +33,4 @@ public class Node<T> {
     public void setNext(Node<T> next) {
         this.next = next;
     }
-
-    private Node<T> prev;
-    private Node<T> next;
-
-    public Node(T data, Node<T> prev, Node<T> next) {
-        this.data = data;
-        this.prev = prev;
-        this.next = next;
-    }
-
 }
