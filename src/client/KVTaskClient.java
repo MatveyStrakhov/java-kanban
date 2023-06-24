8bd1d5807de26d5c14974948c8ebb25de53f9165
MStrakhov <matveystrakhov@yandex.ru>
2023-06-24 13:15:30 +0300

    Project 8 V 0.2 feat:client added

---
+++ src/client/KVTaskClient.java
@@ -0,0 +1,52 @@
+package client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+    private String API_TOKEN;
+    private final String url;
+    private final HttpClient client;
+
+    public KVTaskClient(String url){
+        this.url = url;
+        this.client = HttpClient.newHttpClient();
+        try{
+            URI uri = URI.create(url + "/register");
+            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+            String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            this.API_TOKEN = response;
+        }
+        catch(IOException | InterruptedException e){
+            System.out.println("error while starting server possibly wrong url");
+        }
+    }
+    void put(String key,String json) throws IOException, InterruptedException {
+        URI uri = URI.create(url + "/save/" +key+ "?API_TOKEN=" + API_TOKEN);
+        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).POST(body).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+    String get(String key) throws IOException, InterruptedException {
+        URI uri = URI.create(url + "/load/" +key+ "?API_TOKEN=" + API_TOKEN);
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
+
+
+
+ public void printAPITOCKEN(){
+      System.out.println(API_TOKEN);
+   }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        KVTaskClient client = new KVTaskClient("http://localhost:8078");
+        client.printAPITOCKEN();
+        client.put("test","test");
+        System.out.println(client.get("test"));
+    }
+}
